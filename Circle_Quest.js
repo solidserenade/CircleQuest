42b7fa0ce3ea664a9df62d8698fd1d345d7790c9
solidserenade <solidserenade@yandex.ru>
2019-03-14 14:36:57 +0300

    version_1.0

--- Circle_Quest.js
+++ Circle_Quest.js
@@ -1,29 +1,35 @@
 window.onload = function()
 {
+  let buttonQuantity = 8, // кол-во кнопок
+      buttonArray = [];   // для исключения повторений при нажатии кнопок
 
-  for(let i = 0; i<8; i++)
-  {
+
+  for(let i = 0; i<buttonQuantity; i++)
+  { // вывод кнопок на экран
     createCircle (document.body);
+    buttonArray.push(i);
   }
 
   let btnRow = document.querySelectorAll(".buttons");
-  btnRow.forEach((e)=>{
-    e.onclick = push;
+  btnRow.forEach((elem,index)=>{
+
+    let arrToRandom = buttonArray.slice(0);
+    arrToRandom.splice(index,1);
+
+    let firstRandomNum = arrToRandom.splice(randomInteger(1,arrToRandom.length-1),1);
+    console.log (firstRandomNum, arrToRandom, arrToRandom.length);
+
+    let firstElem = pushBtn.bind(elem);
+    let otherElem = pushBtn.bind(btnRow[firstRandomNum]);
+
+    elem.onclick = ()=>{
+      firstElem();
+      otherElem()
+    };
+    // let otherElem = pushBtn.bind(btnRow[0]);
+    // elem.onclick = otherElem;
   });
 }
-
-// Классы
-// class circles
-// {
-//
-//   constructor(type,cirClass,id)
-//   {
-//     this.type = type;
-//     this.cirClass = cirClass;
-//     this.id = id;
-//   }
-//
-// }
 
 // Функции
 function randomInteger(min, max)
@@ -34,7 +40,7 @@ function randomInteger(min, max)
 }
 
 // Изменение цвета объекта по нажатию
-function push ()
+function pushBtn ()
 {
   this.classList.toggle("tag");
 }
